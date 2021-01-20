0c674425808c41cb178527e5d56fc6f20f4d4b2b
syauqeesy <syauqeesy@gmail.com>
2021-01-20 20:58:52 +0700

    Register done

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -3,9 +3,23 @@ const Controller = require(path.join(__dirname, '../../core/Controller'))
 
 class User extends Controller {
   async register (req, res) {
-    const users = await this.models.user.findAll()
-
-    res.send(users)
+    const { userName, email, password, securityPIN } = req.body
+    try {
+      const salt = await this.modules.bcrypt.genSalt(10)
+      const hashedPassword = await this.modules.bcrypt.hash(password, salt)
+      const result = await this.models.user.create({ userName, email, password: hashedPassword, securityPIN })
+      res.status(201).json({
+        status: 'Success',
+        message: 'Register success!',
+        user: result
+      })
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({
+        status: 'Failed',
+        message: 'Internal server error'
+      })
+    }
   }
 }
 
