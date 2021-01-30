8ea025e9fc3cc1f0914fcff556b5110a2aba3dd6
syauqeesy <syauqeesy@gmail.com>
2021-01-30 13:41:33 +0700

    change table name in migrations

--- src/migrations/20210121125942-create-transaction.js
+++ src/migrations/20210121125942-create-transaction.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('transactions', {
+    await queryInterface.createTable('Transactions', {
       id: {
         type: Sequelize.UUID,
         primaryKey: true,
@@ -34,6 +34,6 @@ module.exports = {
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('transactions')
+    await queryInterface.dropTable('Transactions')
   }
 }
--- src/migrations/20210121131658-create-transfer.js
+++ src/migrations/20210121131658-create-transfer.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('transfers', {
+    await queryInterface.createTable('Transfers', {
       id: {
         type: Sequelize.UUID,
         primaryKey: true,
@@ -26,6 +26,6 @@ module.exports = {
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('transfers')
+    await queryInterface.dropTable('Transfers')
   }
 }
